c7b0048c1ac72d9421473add988085ee201c1396
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-26 14:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+            var selectElement = document.getElementById('colorSelect');
+            var selectedOption = selectElement.options[selectElement.selectedIndex];
+            if (selectedOption) {
+                selectElement.removeChild(selectedOption);
+            }
+}
